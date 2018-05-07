86cb26a63c984690b606ff776cdc0e36a4d55699
theenderweggin <jrstieber@gmail.com>
2018-05-07 14:55:34 -0700

    init front end solidity app

---
+++ lottery-react/src/App.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import web3 from './web3.js';
+import lottery from './lottery';
+
+class App extends Component {
+  state = {
+    manger: '',
+    players: [],
+    balance: '',
+    value: ''
+  };
+
+  async componentDidMount() {
+    const manager = await lottery.methods.manager().call();
+    const players = await lottery.methods.getPlayers().call();
+    const balance = await web3.eth.getBalance(lottery.options.address);
+
+    this.setState({ manager, players, balance });
+  }
+  render() {
+    return (
+      <div>
+        <h2>Lottery Contract</h2>
+        <p>
+          This contract is managed by {this.state.manager}.
+          There are currently {this.state.players.length} people
+          competing to win {web3.utils.fromWei(this.state.balance, 'ether')} ether!
+        </p>
+        <hr />
+        <form>
+          <h4>Want to try your luck?</h4>
+          <div>
+            <label>Amount of ether to enter</label>
+            <input
+              value={this.state.value}
+              onChange={event => this.setState({value: event.target.value})}
+            />
+          </div>
+          <button>Enter</button>
+         </form>
+
+      </div>
+    );
+  }
+}
+
+export default App;
